03d0dc2526ffaa3aaf21daa05ec0122e5d1b69c4
Matheus Almeida <almeida-matheus@hotmail.com.br>
2017-01-10 16:42:24 -0200

    Express server

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ import express from 'express';
 import helmet from 'helmet';
 import compression from 'compression';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import winston from 'winston';
 
 let app = express();
 
 const SERVER_IP = '127.0.0.1';
 const SERVER_PORT = '8080';
+
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser());
+
+app.listen(SERVER_PORT, SERVER_IP, () => {
+	winston.log('info', `Server listering on port: ${SERVER_PORT} IP: ${SERVER_IP}`);
+});
+
+export default app;
